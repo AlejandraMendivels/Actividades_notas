cfe28716ce5e94f5598cea05c745eb19784d4aac
AlejandraMendivels <larairan@uniboyaca.edu.co>
2023-05-26 12:34:24 -0500

    Se agrego accion_modificar_actividades

---
+++ views/accion_modificar_actividades.php
@@ -0,0 +1,22 @@
+<?php
+require '../../models/actividad.php';
+require '../../controllers/conexionDbController.php';
+require '../../controllers/baseController.php';
+require '../../controllers/actividadController.php';
+
+use actividad\Actividad;
+use actividadController\ActividadController;
+
+$actividad = new Actividad();
+$actividad->setId($_POST['id']);
+$actividad->setDescripcion($_POST['descripcion']);
+$actividad->setNota($_POST['nota']);
+
+$actividadController = new ActividadController();
+$resultado = $actividadController->update($actividad->getId(), $actividad);
+if ($resultado) {
+    echo '<h1>Lograste modificar la actividad</h1>';
+} else {
+    echo '<h1>lo siento , no se modificar la actividad </h1>';
+}
+
